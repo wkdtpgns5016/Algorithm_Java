8c6200f51e1360352c6a07081fbbb9a5c40c123b
wkdtpgns5016 <wkdtpgns5016@naver.com>
2021-03-19 15:03:07 +0900

    2021-03-19

---
+++ src/DFS_BFS/Main_11724.java
@@ -0,0 +1,68 @@
+package DFS_BFS;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+// BOJ 11724 - 연결 요소의 개수
+public class Main_11724 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line = br.readLine();
+        StringTokenizer st = new StringTokenizer(line," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        LinkedList<Integer>[] abjList = new LinkedList[n+1];
+        for(int i=0;i<=n;i++){
+            abjList[i] = new LinkedList<Integer>();
+        }
+        boolean[] visited = new boolean[n+1];
+
+        for(int i=0;i<m;i++){
+            line = br.readLine();
+            st = new StringTokenizer(line," ");
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            abjList[u].add(v);
+            abjList[v].add(u);
+        }
+        for(int i=1;i<=n;i++){
+            Collections.sort(abjList[i]);
+        }
+        int result = getConnectedComponent(abjList,visited);
+        bw.write(Integer.toString(result));
+        bw.flush();
+    }
+
+    public static int getConnectedComponent(LinkedList<Integer>[] list, boolean[] visited){
+        int count = 0;
+
+        for(int i=1;i<visited.length;i++){
+            if(!visited[i]) {
+                DFS_list(i,list,visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void DFS_list(int v,LinkedList<Integer>[] list, boolean[] visited){
+        visited[v] = true;
+
+        Iterator<Integer> iter = list[v].listIterator();
+        while(iter.hasNext()){
+            int w = iter.next();
+            if(!visited[w]){
+                DFS_list(w,list,visited);
+            }
+        }
+    }
+}
---
+++ src/DFS_BFS/Main_1260.java
@@ -0,0 +1,76 @@
+package DFS_BFS;
+
+import java.io.*;
+import java.util.*;
+
+// BOJ 1260 - DFS와 BFS
+public class Main_1260 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = br.readLine();
+        StringTokenizer st = new StringTokenizer(line," ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        LinkedList<Integer>[] abjList = new LinkedList[n+1];
+        for(int i=0;i<=n;i++){
+            abjList[i] = new LinkedList<Integer>();
+        }
+        boolean[] visited = new boolean[n+1];
+
+        for(int i=0;i<m;i++){
+            line = br.readLine();
+            st = new StringTokenizer(line, " ");
+
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+
+            abjList[node1].add(node2);
+            abjList[node2].add(node1);
+        }
+        for(int i=1;i<=n;i++){
+            Collections.sort(abjList[i]);
+        }
+        DFS_list(v,abjList,visited);
+        System.out.println();
+        visited = new boolean[n+1];
+        BFS_list(v,abjList,visited);
+    }
+    // DFS 인접리스트 : 재귀 구현
+    public static void DFS_list(int v, LinkedList<Integer>[] list, boolean[] visited){
+        visited[v] = true;
+        System.out.print(v+" ");
+
+        Iterator<Integer> iter = list[v].listIterator();
+        while(iter.hasNext()){
+            int w = iter.next();
+            if(!visited[w]){
+                DFS_list(w,list,visited);
+            }
+        }
+    }
+
+    // BFS 인접리스트 : 큐 구현
+    public static void BFS_list(int v, LinkedList<Integer>[] list, boolean[] visited){
+        Queue<Integer> queue = new LinkedList<>();
+        visited[v] = true;
+        queue.add(v);
+
+        while(!queue.isEmpty()){
+            v = queue.poll();
+            System.out.print(v+" ");
+
+            Iterator<Integer> iter = list[v].listIterator();
+            while(iter.hasNext()){
+                int w = iter.next();
+                if(!visited[w]){
+                    visited[w] = true;
+                    queue.add(w);
+                }
+            }
+        }
+    }
+}
